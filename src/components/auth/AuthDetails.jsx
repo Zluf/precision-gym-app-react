f0a103ed725e26336aa6ee2abb7ee7211202c79a
Slav Konovalov <slav.konovalov@gmail.com>
2025-05-18 17:56:39 +0100

    introduced .prettierrc

--- src/components/auth/AuthDetails.jsx
+++ src/components/auth/AuthDetails.jsx
@@ -1,51 +1,56 @@
-import React, { useEffect } from "react";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from "../../firebase";
-import AppContext from "../../context/app-context";
+import React, { useEffect } from 'react'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../../firebase'
+import AppContext from '../../context/app-context'
 
 export default function AuthDetails() {
-  const context = React.useContext(AppContext);
+     const context = React.useContext(AppContext)
 
-  const userSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        fetch(
-          `https://precision-gym-default-rtdb.firebaseio.com/users/guest.json`,
-          {
-            method: "DELETE",
-          }
-        );
+     const userSignOut = () => {
+          signOut(auth)
+               .then(() => {
+                    fetch(
+                         `https://precision-gym-default-rtdb.firebaseio.com/users/guest.json`,
+                         {
+                              method: 'DELETE',
+                         }
+                    )
 
-        console.log("Sign Out Successful 🔒");
-      })
-      .catch((error) => console.log(error));
-  };
+                    console.log('Sign Out Successful 🔒')
+               })
+               .catch((error) => console.log(error))
+     }
 
-  // Signs out guest on the next page load
-  useEffect(() => {
-    window.addEventListener("beforeunload", () => {
-      if (auth.currentUser && auth.currentUser.displayName === "guest") {
-        userSignOut(auth);
-      }
-    });
+     // Signs out guest on the next page load
+     useEffect(() => {
+          window.addEventListener('beforeunload', () => {
+               if (
+                    auth.currentUser &&
+                    auth.currentUser.displayName === 'guest'
+               ) {
+                    userSignOut(auth)
+               }
+          })
 
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        context.setUser(
-          user.displayName ? user.displayName : user.email.split("@")[0]
-        );
-      } else context.setUser(null);
-    });
-  }, []);
+          onAuthStateChanged(auth, (user) => {
+               if (user) {
+                    context.setUser(
+                         user.displayName
+                              ? user.displayName
+                              : user.email.split('@')[0]
+                    )
+               } else context.setUser(null)
+          })
+     }, [])
 
-  return (
-    <div className="auth-details">
-      {context.authUser && (
-        <>
-          <p>{`Signed in as ${context.authUser}`}</p>
-          <button onClick={userSignOut}>Sign Out</button>
-        </>
-      )}
-    </div>
-  );
+     return (
+          <div className="auth-details">
+               {context.authUser && (
+                    <>
+                         <p>{`Signed in as ${context.authUser}`}</p>
+                         <button onClick={userSignOut}>Sign Out</button>
+                    </>
+               )}
+          </div>
+     )
 }
